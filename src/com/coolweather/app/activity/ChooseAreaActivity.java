790cea96a7ffd610254a3636144c9f916e391cd9
rmf <304670045@qq.com>
2016-09-02 18:06:59 +0800

    回家继续~

---
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,122 @@
+package com.coolweather.app.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.coolweather.app.R;
+import com.coolweather.app.db.CoolWeatherDB;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ChooseAreaActivity extends Activity {
+
+	public static final int LEVEL_PROVINCE = 0;
+	public static final int LEVEL_CITY = 1;
+	public static final int LEVEL_COUNTY = 2;
+
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private CoolWeatherDB coolWeatherDB;
+	private List<String> dataList = new ArrayList<String>();
+
+	private List<Province> provinceList;
+	private List<City> cityList;
+	private List<County> countyList;
+	private Province selectedProvince;
+	private City selectedCity;
+	private int currentLevel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.choose_area);
+		titleText = (TextView) findViewById(R.id.list_view);
+		listView = (ListView) findViewById(R.id.list_view);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, dataList);
+		listView.setAdapter(adapter);
+		coolWeatherDB = CoolWeatherDB.getInstance(this);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (currentLevel == LEVEL_PROVINCE) {
+					selectedProvince = provinceList.get(position);
+					queryCities();
+				} else if (currentLevel == LEVEL_CITY) {
+					selectedCity = cityList.get(position);
+					queryCounties();
+				}
+			}
+		});
+		queryProvinces();
+	}
+
+	private void queryProvinces() {
+		provinceList = coolWeatherDB.loadProvinces();
+		if (provinceList.size() > 0) {
+			dataList.clear();
+			for (Province province : provinceList) {
+				dataList.add(province.getProvinceName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText("ÖÐ¹ú");
+			currentLevel = LEVEL_PROVINCE;
+		} else {
+			queryFromServer(null, "province");
+		}
+	}
+
+	private void queryCities() {
+		cityList = coolWeatherDB.loadCities(selectedProvince.getId());
+		if (cityList.size() > 0) {
+			dataList.clear();
+			for (City city : cityList) {
+				dataList.add(city.getCityName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedProvince.getProvinceName());
+			currentLevel = LEVEL_CITY;
+		} else {
+			queryFromServer(selectedProvince.getProvinceCode(), "city");
+		}
+	}
+
+	private void queryCounties() {
+		countyList = coolWeatherDB.loadCounties(selectedCity.getId());
+		if (countyList.size() > 0) {
+			dataList.clear();
+			for (County county : countyList) {
+				dataList.add(county.getCountyName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedCity.getCityName());
+			currentLevel = LEVEL_COUNTY;
+		} else {
+			queryFromServer(selectedCity.getCityCode(), "county");
+		}
+	}
+
+	private void queryFromServer(Object object, String string) {
+
+	}
+
+}
